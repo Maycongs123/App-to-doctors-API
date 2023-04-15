116cd31db56edd3ff979744bd92c977b81ebc67a
maycon.santos <maycongs123@gmail.com>
2023-04-15 01:11:24 -0300

    criacao metodos bookcontroller

--- BookAPI/Controllers/BooksController.cs
+++ BookAPI/Controllers/BooksController.cs
@@ -4,13 +4,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookAPI.Controllers
 {
-    public class BooksController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
     {
         private readonly IBookRepository _bookRepository;
 
         public BooksController(IBookRepository bookRepository)
         {
-            _bookRepository = bookRepository; 
+            _bookRepository = bookRepository;
         }
 
         [HttpGet]
@@ -19,5 +21,44 @@ namespace BookAPI.Controllers
             return _bookRepository.GetAll();
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Books>> GetBookById(int id)
+        {
+            return await _bookRepository.GetById(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Books>> PostBooks(Books book)
+        {
+            Books newBook = await _bookRepository.Create(book);
+
+            return CreatedAtAction(nameof(GetBooks), new {id = newBook.Id}, newBook);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Books>> Delete(int id)
+        {
+            Books bookToDelete = await _bookRepository.GetById(id);
+
+            if (bookToDelete == null)
+            {
+                return NotFound();
+
+            }
+            await _bookRepository.Delete(bookToDelete.Id);
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Books>> PutBooks([FromBody] Books book, int id)
+        {
+            if (id == book.Id)
+            {
+                return BadRequest();
+
+            }
+            await _bookRepository.Update(book);
+            return NoContent();
+        }
     }
 }
