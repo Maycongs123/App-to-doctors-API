9516aa75a844829a8e829a89910018982d455ab5
Diego Nobre <diegonobre.dev@outlook.com>
2023-07-27 17:48:35 -0300

    Criação de novas propriedades e tabelas no SQL

---
+++ BookAPI/Migrations/20230726204205_criacaoDeNovasColunas.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class criacaoDeNovasColunas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Administracao",
+                table: "Medicamentos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Dose",
+                table: "Medicamentos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PreparoDiluicao",
+                table: "Medicamentos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsoGestacao",
+                table: "Medicamentos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Administracao",
+                table: "Medicamentos");
+
+            migrationBuilder.DropColumn(
+                name: "Dose",
+                table: "Medicamentos");
+
+            migrationBuilder.DropColumn(
+                name: "PreparoDiluicao",
+                table: "Medicamentos");
+
+            migrationBuilder.DropColumn(
+                name: "UsoGestacao",
+                table: "Medicamentos");
+        }
+    }
+}
--- BookAPI/Models/Medicamento.cs
+++ BookAPI/Models/Medicamento.cs
@@ -5,19 +5,40 @@ namespace DoctorAPI.Models
     public class Medicamento
     {
         public int Id { get; set; }
+
         public string? Nome { get; set; }
+
         public string? MedicamentoUso { get; set; }
+
         public string? Tipo { get; set; }
+
         public string? dosagemTipo { get; set; }
+
         public string? ModoDeUso { get; set; }
+
         public string? QuantidadeMg { get; set; }
+
         public string? QuantidadeMl { get; set; }
+
         public float? QuantidadeMgKg { get; set; }
+
         public float? QuantidadeSoro { get; set; }
+
         public string? Indicacao { get; set; }
+
         public string? ContraIndicacao { get; set; }
+
         public float? NumeroDoses { get; set; }
+
         public float? QuantidadeAmpolas { get; set; }
+
+        public string? Dose { get; set; }
+
+        public string? PreparoDiluicao { get; set; }
+
+        public string? Administracao { get; set; }
+
+        public string? UsoGestacao { get; set; }
 
     }  
 }
