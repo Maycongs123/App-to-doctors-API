a2c25a049160459a000250a83c3c934b174e25c1
Maycon gonçalves <maycongs123@gmail.com>
2023-10-13 11:38:06 -0300

    ajuste para commit

--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -92,7 +92,6 @@ builder.Services.AddScoped<ILoginRepository, LoginRepository>();
 builder.Services.AddScoped<ITokenService, TokenService>();
 
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
